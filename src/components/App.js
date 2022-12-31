1ad9563480616221ec6c2365c3cc53d2bcf5c828
Yijin Ro <yri.carmine@gmail.com>
2022-12-31 14:41:09 -0500

    added if statement and command  for Alan Ai voice command

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,36 @@
 import { Button } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web'
 
 /**
  * This is the entry point for all of our react stuff
  */
 const App = () => {
+
+    useEffect(()=>{
+        alanBtn({
+          key: "8a8c89673b8fe90f10f216aa79d578972e956eca572e1d8b807a3e2338fdd0dc/stage",
+          onCommand: ({ command, data }) => {
+            if (command === "navigate") {
+              window.open("www.google.com")
+            }
+            if(command === "weather"){
+                console.log('loading weather data')
+            }
+            if(command === "joke"){
+                console.log('loading deveolper joke')
+            }
+            if(command === "date"){
+                console.log(data);
+            }
+          },
+        });
+    }, [])
+
     return (
         <div>
-            <h1>Welcome to dsfghjkl, Good luck!</h1>
-            <Button variant="contained">Hello World</Button>
+            <h1>How can I help you?</h1>
+            <Button variant="contained">Start</Button>
         </div>
     );
 };
